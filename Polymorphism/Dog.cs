52ac8b50a886541f49a22788ce956319c2afe69c
hello <hello@LAPTOP-NA2F1EVT>
2021-03-19 12:07:27 -0400

    Add project files.

---
+++ Polymorphism/Dog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Polymorphism
+{
+    class Dog : Animal
+    {
+        // Field
+        private string _name;
+
+ // Constructor
+public Dog(string name)
+: base("Dog")
+        {
+            _name = name;
+        }
+        // Name property
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        // MakeSound method
+        public override void MakeSound()
+        {
+            MessageBox.Show("Woof! Woof!");
+        }
+
+    }
+}
+  
